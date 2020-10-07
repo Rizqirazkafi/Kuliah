071bea23a0bca45962c7209faadef90aa247e2ed
M. Rizqi R <50984050+Rizqirazkafi@users.noreply.github.com>
2020-10-07 08:45:51 +0700

    Source code Pemrograman Dasar
    Source code Pemrograman Dasar Semester 1 Tugas 4 dan LKM 5

---
+++ PD4-5/2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int a ;
+
+    cout << "Masukkan angka antara 0 sampai 9" << endl;
+    cin >> a;
+    switch(a){
+        case 0 : cout << "nol" << endl;
+                    break ;
+
+        case 1 : cout << "satu" << endl;
+                    break ;
+                    
+        case 2 : cout << "dua" << endl;
+                    break ;
+
+        case 3 : cout << "tiga" << endl;
+                    break ;
+
+        case 4 : cout << "empat" << endl;
+                    break ;
+
+        case 5 : cout << "lima" << endl;
+                    break ;
+
+        case 6 : cout << "enam" << endl;
+                    break ;
+
+        case 7 : cout << "tujuh" << endl;
+                    break ;
+
+        case 8 : cout << "delapan" << endl;
+                    break ;
+
+        case 9 : cout << "sembilan" << endl;
+                    break ;
+
+            default : cout << "Angka yang anda masukkan lebih dari 9 atau kurang dari 0 atau bukan angka." << endl;
+    }
+}
