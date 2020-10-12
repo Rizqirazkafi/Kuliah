02bf6dc21efaad3174b8c4c66e78361678f1d266
Rizqirazkafi <rizqirazkafi56@gmail.com>
2020-10-12 20:54:25 +0700

    add opsi 4

--- Pemrograman Dasar/PD6-7/deret-bilangan.cpp
+++ Pemrograman Dasar/PD6-7/deret-bilangan.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     string final;
-    int cacah , genap , gasal , fibonanci , opsi;
+    int cacah, genap, gasal, n, a, fib1 = 1, fib2 = 0, opsi ;
 
     while (final != "n")
     {
@@ -14,41 +14,60 @@ int main()
         cout << "1. Bilangan cacah " << endl;
         cout << "2. Bilangan genap " << endl;
         cout << "3. Bilangan gasal " << endl;
-        cout << "4. Bilangan fibonanci " << endl;
+        cout << "4. Bilangan fibonacci " << endl;
 
         cout << endl;
 
         cout << "Masukkan pilihan : ";
         cin >> opsi;
 
-        if (opsi == 1){
-            for (cacah = 0 ; cacah <= 20; ){
-                
+        if (opsi == 1)
+        {
+            for (cacah = 0; cacah <= 20;)
+            {
+
                 cout << cacah << "\n";
-                cacah ++;
+                cacah++;
             }
             cout << "Tampilkan bilangan lainnya ? (y/n) : ";
             cin >> final;
-            
-        }else if (opsi == 2){
-            for (genap = 0 ; genap <= 20 ; genap++){
+        }
+        else if (opsi == 2)
+        {
+            for (genap = 0; genap <= 20; genap++)
+            {
                 // genap = genap + 2;
                 cout << genap << "\n";
-                genap ++;
+                genap++;
             }
             cout << "Tampilkan bilangan lainnya ? (y/n) : ";
             cin >> final;
-        }else if (opsi == 3){
-            for (gasal = 0 ; gasal <= 20 ; gasal++){
+        }
+        else if (opsi == 3)
+        {
+            for (gasal = 0; gasal <= 20; gasal++)
+            {
                 gasal = gasal + 1;
                 cout << gasal << "\n";
-                
+            }
+            cout << "Tampilkan bilangan lainnya ? (y/n) : ";
+            cin >> final;
+        }
+        else if (opsi == 4)
+        {
+            cout << "masukkan n (batas akhir): ";
+            cin >> n;
+            for (a = 1; a <= n; ++a)
+            {
+                a = fib1 + fib2;
+                fib2 = fib1;
+                fib1 = a;
+                cout << a << "\n";
             }
             cout << "Tampilkan bilangan lainnya ? (y/n) : ";
             cin >> final;
         }
     }
 }
-
 
 // Credit to Rizqi R
