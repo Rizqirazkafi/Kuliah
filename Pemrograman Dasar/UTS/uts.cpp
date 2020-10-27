f4d87f7222100971398f94ccc5d93f0a6a2c6e47
Rizqirazkafi <rizqirazkafi56@gmail.com>
2020-10-27 22:26:06 +0700

    Added repetition confirmation

--- Pemrograman Dasar/UTS/uts.cpp
+++ Pemrograman Dasar/UTS/uts.cpp
@@ -1,9 +1,8 @@
-#include <conio.h>
 #include <algorithm>
+#include <conio.h>
 #include <iostream>
 #include <math.h>
 #include <string>
-
 
 using namespace std;
 
@@ -11,73 +10,92 @@ int main() {
   unsigned long long des_num; // bilangan yang akan di konversikan
   int pil;
 
-  string bits;
+  string bits, final;
 
-  cout << "1. Desimal ke Binari" << endl;
-  cout << "2. Desimal ke Oktal" << endl;
-  cout << "3. Binari ke Desimal" << endl;
-  cout << "4. Oktal ke Desimal" << endl;
-  cout << "=====================" << endl;
-  cout << " Pilih opsi konversi : ";
-  cin >> pil;
+  while (final != "n" ) {
 
-  if (pil == 1) {
-    cout << " Masukkan bilangan desimal (Maksimal 65535) : ";
-    cin >> des_num;
-    // Desimal ke Binari
-    while (des_num > 0) {
-      bits.append(to_string(des_num % 2));
-      des_num /= 2;
-    }
+    cout << "1. Desimal ke Binari" << endl;
+    cout << "2. Desimal ke Oktal" << endl;
+    cout << "3. Binari ke Desimal" << endl;
+    cout << "4. Oktal ke Desimal" << endl;
+    cout << "=====================" << endl;
+    cout << " Pilih opsi konversi : ";
+    cin >> pil;
 
-    reverse(bits.begin(), bits.end());
-    cout << bits << endl;
-  } else if (pil == 2) {
-    // Desimal ke Oktal
-    cout << " Masukkan bilangan desimal (Maksimal 65535) : ";
-    cin >> des_num;
-    int oct_num = 0, awal = 1;
-
-    while (des_num != 0) {
-      oct_num += (des_num % 8) * awal;
-      des_num /= 8;
-      awal *= 10;
-    }
-
-    cout << oct_num << endl;
-  } else if (pil == 3) {
-    // Binari ke Desimal
-    cout << " Masukkan bilangan binari (Maksimal 16 bit) : ";
-    cin >> bits;
-    des_num = 0;
-    reverse(bits.begin(), bits.end());
-    int size = bits.size();
-
-    for (int i = 0; i < size; i++) {
-      if (bits[i] == '1') {
-        des_num += pow(2, i);
+    if (pil == 1) {
+      cout << " Masukkan bilangan desimal (Maksimal 65535) : ";
+      cin >> des_num;
+      // Desimal ke Binari
+      while (des_num > 0) {
+        bits.append(to_string(des_num % 2));
+        des_num /= 2;
       }
-    }
 
-    cout << "Nilai desimalnya ialah : " << des_num << endl;
-  } else if (pil == 4) {
-    // Oktal ke Desimal
-    long int oct_num, dec_num = 0;
-    int i = 0;
-    cout << "Masukkan bilangan oktal (Maksimal 177777) : ";
-    cin >>oct_num;
+      reverse(bits.begin(), bits.end());
+      cout << bits << endl;
 
-    while(oct_num != 0){
-      dec_num = dec_num + (oct_num % 10) * pow(8, i);
-      i++;
-      oct_num /= 10;
+      cout << "Lakukan konversi lagi ? (y/n) : ";
+      cin >> final;
+
+    } else if (pil == 2) {
+      // Desimal ke Oktal
+      cout << " Masukkan bilangan desimal (Maksimal 65535) : ";
+      cin >> des_num;
+      int oct_num = 0, awal = 1;
+
+      while (des_num != 0) {
+        oct_num += (des_num % 8) * awal;
+        des_num /= 8;
+        awal *= 10;
+      }
+
+      cout << oct_num << endl;
+
+      cout << "Lakukan konversi lagi ? (y/n) : ";
+      cin >> final;
+
+    } else if (pil == 3) {
+      // Binari ke Desimal
+      cout << " Masukkan bilangan binari (Maksimal 16 bit) : ";
+      cin >> bits;
+      des_num = 0;
+      reverse(bits.begin(), bits.end());
+      int size = bits.size();
+
+      for (int i = 0; i < size; i++) {
+        if (bits[i] == '1') {
+          des_num += pow(2, i);
+        }
+      }
+
+      cout << "Nilai desimalnya ialah : " << des_num << endl;
+
+      cout << "Lakukan konversi lagi ? (y/n) : ";
+      cin >> final;
+
+    } else if (pil == 4) {
+      // Oktal ke Desimal
+      long int oct_num, dec_num = 0;
+      int i = 0;
+      cout << "Masukkan bilangan oktal (Maksimal 177777) : ";
+      cin >> oct_num;
+
+      while (oct_num != 0) {
+        dec_num = dec_num + (oct_num % 10) * pow(8, i);
+        i++;
+        oct_num /= 10;
+      }
+      cout << "Nilai desimalnya adalah : " << dec_num << endl;
+      getch();
+
+      cout << "Lakukan konversi lagi ? (y/n) : ";
+      cin >> final;
+
+    } else {
+
+      cout << "Nilai yang anda masukkan salah" << endl;
+      cout << "Ulangi ? (y/n) :";
     }
-     cout << "Nilai desimalnya adalah : " << dec_num << endl;
-     getch();
-  } else { 
-    
-    cout << "Nilai yang anda masukkan salah"  << endl;
-  
   }
 
   // Desimal ke Oktal
