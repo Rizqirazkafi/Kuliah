2339b07d27197c1ff641107538a0453bd15231aa
Rizqirazkafi <rizqirazkafi56@gmail.com>
2020-11-15 18:13:15 +0700

    code jam

--- Pemrograman Dasar/PD9-10/lkm1.cpp
+++ Pemrograman Dasar/PD9-10/lkm1.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
+  int n;
+  int num[n];
 
-  int num[10];
+  cout << "Masukkan jumlah deret pertama : ";
+  cin >> n;
   // membuat deret
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < n; ++i) {
     if (i < 2)
       num[i] = i;
 
@@ -14,7 +17,7 @@ int main() {
   }
   // menampilkan deret
   cout << "berikut adalah 10 deret fibonacci pertama: \n";
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < n; ++i) {
     cout << num[i] << " ";
   }
   cout << endl;
