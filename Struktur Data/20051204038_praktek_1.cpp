d174dad4fa6e502111dad734830a2e22fb07d9ac
Rizqirazkafi <rizqirazkafi56@gmail.com>
2021-02-12 20:17:06 +0700

    Fixed Alfito

--- Struktur Data/20051204038_praktek_1.cpp
+++ Struktur Data/20051204038_praktek_1.cpp
@@ -1,60 +1,58 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-int main ()
-{
-    char ulang = 'Y';
-    while (ulang == 'Y' || ulang == 'y')
-{
+int main() {
+  char ulang = 'Y';
+  while (ulang == 'Y' || ulang == 'y') {
 
-    struct data
-    {
-        char nama[20];
-        long long int nim;
-        int tugas;
-        int uts;
-        int uas;
-        int partisipasi;
-        int cal;
-        int no;
+    struct data {
+      char nama[20];
+      long long int nim;
+      int tugas;
+      int uts;
+      int uas;
+      int partisipasi;
+      int cal;
+      int no;
     };
     struct data mahasiswa[100];
-    int x,i,cal;
-    cout << "====================================\n";
-    cout << "===========ALFITO MULYONO===========\n";
-    cout << "====================================\n";
-    cout<<"Masukan Jumlah Mahasiswa : ";
-    cin>>i;
-    for(x=0;x<i;x++)
-{
-    cout<<"\ninput MAHASISWA KE? : " ;
-    cin>>mahasiswa[x].no;
-    cout<<"Masukan Nama Mahasiswa      : ";
-    cin>>mahasiswa[x].nama;
-    cout<<"Masukan NIM Mahasiswa       : ";
-    cin>>mahasiswa[x].nim;
-    cout<<"Masukan Nilai Tugas         : ";
-    cin>>mahasiswa[x].tugas;
-    cout<<"Masukan Nilai Partisipasi   : ";
-    cin>>mahasiswa[x].partisipasi;
-    cout<<"Masukan Nilai UTS           : ";
-    cin>>mahasiswa[x].uts;
-    cout<<"Masukan Nilai UAS           : ";
-    cin>>mahasiswa[x].uas;
-        cal=(mahasiswa[x].tugas*0.3)+( mahasiswa[x].partisipasi*0.2) +( mahasiswa[x].uts*0.2) +(mahasiswa[x].uas*0.3);
-    cout<<"Nilai Akhir                 : "<<cal << "\n\n";
-}
+    int x, i, tot;
+    cout << "Rekap Nilai" << endl;
+    cout << "Masukan Jumlah Mahasiswa : ";
+    cin >> i;
+    for (x = 0; x < i; x++) {
+      cout << endl;
+      cout << "input mahasiswa ke           : ";
+      cin >> mahasiswa[x].no;
+      cout << "Masukan Nama Mahasiswa       : ";
+      cin >> mahasiswa[x].nama;
+      cout << "Masukan NIM Mahasiswa        : ";
+      cin >> mahasiswa[x].nim;
+      cout << "Masukan Nilai Tugas          : ";
+      cin >> mahasiswa[x].tugas;
+      cout << "Masukan Nilai Partisipasi    : ";
+      cin >> mahasiswa[x].partisipasi;
+      cout << "Masukan Nilai UTS            : ";
+      cin >> mahasiswa[x].uts;
+      cout << "Masukan Nilai UAS            : ";
+      cin >> mahasiswa[x].uas;
+      tot = (mahasiswa[x].tugas * 0.3) + (mahasiswa[x].partisipasi * 0.2) +
+            (mahasiswa[x].uts * 0.2) + (mahasiswa[x].uas * 0.3);
+      cout << "Nilai Akhir                  : " << cal << endl;
+    }
 
-for(x=0;x<i;x++)
-{
-    cout<<"\nMahasiswa ke     : "<<mahasiswa[x].no << "\n";
-    cout<<"Nama Mahasiswa   : "<<mahasiswa[x].nama << "\n";
-    cout<<"Nim Mahasiswa    : "<<mahasiswa[x].nim << "\n";
-    cout<<"Nilai Tugas      : "<<mahasiswa[x].tugas << "\n";
-    cout<<"Nilai UTS        : "<<mahasiswa[x].uts << "\n";
-    cout<<"Nilai UAS        : "<<mahasiswa[x].uas << "\n\n";
-}
-    cout << "Gunakan Lagi Program? Y/N\n";
+    for (x = 0; x < i; x++) {
+      cout << endl;
+      cout << "Mahasiswa ke     : " << mahasiswa[x].no << endl;
+      cout << "Nama Mahasiswa   : " << mahasiswa[x].nama << endl;
+      cout << "Nim Mahasiswa    : " << mahasiswa[x].nim << endl;
+      cout << "Nilai Tugas      : " << mahasiswa[x].tugas << endl;
+      cout << "Nilai UTS        : " << mahasiswa[x].uts << endl;
+      cout << "Nilai UAS        : " << mahasiswa[x].uas << endl;
+      cout << "Nilai Akhir      : " << cal << endl;
+    }
+    cout << endl;
+    cout << "Gunakan Lagi Program? Y/N : ";
     cin >> ulang;
-}
+  }
 }
